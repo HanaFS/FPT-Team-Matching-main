6e87e698467234bfa0a81038ad74c621bc87e1bd
GiHana <156551189+HanaFS@users.noreply.github.com>
2025-03-24 10:13:56 +0700

    Add files via upload

---
+++ CommentRepository.java
@@ -0,0 +1,10 @@
+package ut.edu.teammatching.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ut.edu.teammatching.models.Comment;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+}
